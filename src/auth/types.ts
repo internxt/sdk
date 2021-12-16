a327638667e29496b40545d221015356736fa9bb
sandro <devspuig@pm.me>
2021-12-16 16:05:49 +0100

    Reformat code to match specifications

--- src/auth/types.ts
+++ src/auth/types.ts
@@ -2,18 +2,18 @@ export type Password = string;
 export type Email = string;
 
 export interface LoginDetails {
-    email: Email,
-    password: Password,
-    tfaCode: string | undefined
+  email: Email,
+  password: Password,
+  tfaCode: string | undefined
 }
 
 export interface Keys {
-    privateKeyEncrypted: string,
-    publicKey: string,
-    revocationCertificate: string
+  privateKeyEncrypted: string,
+  publicKey: string,
+  revocationCertificate: string
 }
 
 export interface CryptoProvider {
-    encryptPassword: (password: Password, encryptedSalt: string) => Password,
-    generateKeys: (password: Password) => Keys,
+  encryptPassword: (password: Password, encryptedSalt: string) => Password,
+  generateKeys: (password: Password) => Keys,
 }
--- src/shared/types/userSettings.ts
+++ src/shared/types/userSettings.ts
@@ -1,25 +1,25 @@
 import {AppSumoDetails} from './appsumo';
 
 export interface UserSettings {
-    bucket: string;
-    backupsBucket: string | null;
-    createdAt: Date;
-    credit: number;
-    email: string;
-    lastname: string;
-    mnemonic: string;
-    name: string;
-    privateKey: string;
-    publicKey: string;
-    registerCompleted: boolean;
-    revocationKey: string;
-    root_folder_id: number;
-    userId: string;
-    uuid: string;
-    teams?: boolean;
-    username: string;
-    bridgeUser: string;
-    sharedWorkspace: boolean;
-    appSumoDetails: AppSumoDetails | null;
-    hasReferralsProgram: boolean;
+  bucket: string;
+  backupsBucket: string | null;
+  createdAt: Date;
+  credit: number;
+  email: string;
+  lastname: string;
+  mnemonic: string;
+  name: string;
+  privateKey: string;
+  publicKey: string;
+  registerCompleted: boolean;
+  revocationKey: string;
+  root_folder_id: number;
+  userId: string;
+  uuid: string;
+  teams?: boolean;
+  username: string;
+  bridgeUser: string;
+  sharedWorkspace: boolean;
+  appSumoDetails: AppSumoDetails | null;
+  hasReferralsProgram: boolean;
 }
