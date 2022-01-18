f3b15a6b5122e27a1172f6711a466e74ff97a0fa
sandro <devspuig@pm.me>
2022-01-18 11:30:59 +0100

    Remove unused code

--- src/drive/shared/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { ApiUrl } from '../../shared';
-
-export function getDriveAxiosClient(apiUrl: ApiUrl) {
-  return axios.create({
-    baseURL: apiUrl + '/api',
-  });
-}
