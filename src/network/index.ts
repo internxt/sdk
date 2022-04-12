86cc97003beb268880b07e026f2d019d8c037859
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-04-12 16:35:26 +0200

    fix(network): change upload index type to number

--- src/network/index.ts
+++ src/network/index.ts
@@ -34,9 +34,9 @@ export class Network {
     };
   }
 
-  public startUpload(idBucket: string, payload: StartUploadPayload): Promise<StartUploadResponse> {
+  public startUpload(bucketId: string, payload: StartUploadPayload): Promise<StartUploadResponse> {
     for (const { index, size } of payload.uploads) {
-      if (!isHexString(index) || index.length !== 64) {
+      if (index < 0) {
         throw new Error('Invalid index');
       }
       if (size < 0) {
@@ -50,7 +50,7 @@ export class Network {
     });
   }
 
-  public finishUpload(idBucket: string, payload: FinishUploadPayload): Promise<FinishUploadResponse> {
+  public finishUpload(bucketId: string, payload: FinishUploadPayload): Promise<FinishUploadResponse> {
     const { index, shards } = payload;
     if (!isHexString(index) || index.length !== 64) {
       throw new Error('Invalid index');
@@ -62,23 +62,23 @@ export class Network {
       }
     }
 
-    return Network.finishUpload(idBucket, payload, {
+    return Network.finishUpload(bucketId, payload, {
       client: this.client,
       appDetails: this.appDetails,
       auth: this.auth,
     });
   }
 
-  public getDownloadLinks(idBucket: string, fileId: string): Promise<GetDownloadLinksResponse> {
-    return Network.getDownloadLinks(idBucket, fileId, {
+  public getDownloadLinks(bucketId: string, fileId: string): Promise<GetDownloadLinksResponse> {
+    return Network.getDownloadLinks(bucketId, fileId, {
       client: this.client,
       appDetails: this.appDetails,
       auth: this.auth,
     });
   }
 
-  public async deleteFile(idBucket: string, fileId: string): Promise<void> {
-    await Network.deleteFile(idBucket, fileId, {
+  public async deleteFile(bucketId: string, fileId: string): Promise<void> {
+    await Network.deleteFile(bucketId, fileId, {
       client: this.client,
       appDetails: this.appDetails,
       auth: this.auth,
@@ -87,55 +87,55 @@ export class Network {
 
   /**
    * Creates entries for every upload in the request, returns the urls to upload
-   * @param idBucket
+   * @param bucketId
    * @param uploads
    */
-  private static startUpload(
-    idBucket: string,
+  static startUpload(
+    bucketId: string,
     payload: StartUploadPayload,
     { client, appDetails, auth }: NetworkRequestConfig,
   ) {
     const headers = Network.headersWithBasicAuth(appDetails, auth);
-    return client.post<StartUploadResponse>(`/v2/buckets/${idBucket}/files/start`, payload, headers);
+    return client.post<StartUploadResponse>(`/v2/buckets/${bucketId}/files/start`, payload, headers);
   }
 
   /**
    * Finishes the upload of a file
-   * @param idBucket
+   * @param bucketId
    * @param index
    * @param shards
    */
   private static finishUpload(
-    idBucket: string,
+    bucketId: string,
     payload: FinishUploadPayload,
     { client, appDetails, auth }: NetworkRequestConfig,
   ) {
     const headers = Network.headersWithBasicAuth(appDetails, auth);
-    return client.post<FinishUploadResponse>(`/v2/buckets/${idBucket}/files/finish`, payload, headers);
+    return client.post<FinishUploadResponse>(`/v2/buckets/${bucketId}/files/finish`, payload, headers);
   }
 
   /**
    * Gets the download links for a file
-   * @param idBucket
+   * @param bucketId
    * @param file
    */
   private static getDownloadLinks(
-    idBucket: string,
+    bucketId: string,
     fileId: string,
     { client, appDetails, auth }: NetworkRequestConfig,
   ) {
     const headers = Network.headersWithBasicAuth(appDetails, auth);
-    return client.get<GetDownloadLinksResponse>(`/v2/buckets/${idBucket}/files/${fileId}/mirrors`, headers);
+    return client.get<GetDownloadLinksResponse>(`/v2/buckets/${bucketId}/files/${fileId}/mirrors`, headers);
   }
 
   /**
    * Deletes a file
-   * @param idBucket
+   * @param bucketId
    * @param file
    */
-  private static deleteFile(idBucket: string, fileId: string, { client, appDetails, auth }: NetworkRequestConfig) {
+  private static deleteFile(bucketId: string, fileId: string, { client, appDetails, auth }: NetworkRequestConfig) {
     const headers = Network.headersWithBasicAuth(appDetails, auth);
-    return client.delete(`/v2/buckets/${idBucket}/files/${fileId}`, headers);
+    return client.delete(`/v2/buckets/${bucketId}/files/${fileId}`, headers);
   }
 
   /**
