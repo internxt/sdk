e4fcc3e5bbca07358e141536a4078821aa8adaa9
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-04-21 13:33:32 +0200

    feat(network): add upload/download types

--- src/network/types.ts
+++ src/network/types.ts
@@ -1,31 +1,32 @@
 import { AppDetails } from 'src/shared';
 import { HttpClient } from '../shared/http/client';
 import { BasicAuth } from '../auth/types';
+
+type Hash = string;
 export type BridgeUrl = string;
 
 export interface Shard {
   uuid: string;
-  hash: string;
+  hash: Hash;
 }
 
 export interface StartUploadResponse {
-  uploads: { index: number; uuid: string; url: string }[];
+  uploads: { index: number; uuid: Shard['uuid']; url: string }[];
 }
-
-export interface FinishUploadResponse {
+export type FinishUploadResponse = BucketEntry;
+export interface BucketEntry {
   id: string;
   index: string;
-  frame: string;
   bucket: string;
   name: string;
   mimetype: string;
   created: Date;
 }
 
-interface ShardResponse {
-  index: string;
+export interface DownloadableShard {
+  index: number;
   size: number;
-  hash: string;
+  hash: Shard['hash'];
   url: string;
 }
 
@@ -33,7 +34,9 @@ export interface GetDownloadLinksResponse {
   bucket: string;
   index: string;
   created: Date;
-  shards: ShardResponse[];
+  shards: DownloadableShard[];
+  version?: number;
+  size: number;
 }
 
 export interface NetworkRequestConfig {
@@ -55,3 +58,56 @@ export type FinishUploadPayload = {
   index: string;
   shards: Shard[];
 };
+
+export type UploadFileFunction = (url: string) => Promise<Hash>;
+export type DownloadFileFunction = (
+  downloadables: DownloadableShard[],
+  fileSize: number
+) => Promise<void>;
+
+export type BinaryData = {
+  slice: (from: number, to: number) => BinaryData;
+  toString(encoding: 'hex'): string;
+}
+
+export enum BinaryDataEncoding {
+  HEX = 'hex'
+}
+
+export type ToBinaryDataFunction = (input: string, encoding: BinaryDataEncoding) => BinaryData;
+
+
+export enum SymmetricCryptoAlgorithm {
+  AES256CTR = 'AES256CTR'
+}
+
+export type Algorithm = {
+  type: SymmetricCryptoAlgorithm
+  ivSize: number
+};
+
+export const ALGORITHMS: Record<SymmetricCryptoAlgorithm, Algorithm> = {
+  [SymmetricCryptoAlgorithm.AES256CTR]: {
+    type: SymmetricCryptoAlgorithm.AES256CTR,
+    ivSize: 32
+  }
+};
+
+export type Crypto = {
+  algorithm: Algorithm;
+  randomBytes: (bytesLength: number) => BinaryData;
+  generateFileKey: (mnemonic: string, bucketId: string, index: BinaryData | string) => Promise<BinaryData>;
+}
+
+export type EncryptFileFunction = (
+  algorithm: SymmetricCryptoAlgorithm,
+  key: BinaryData,
+  iv: BinaryData
+) => Promise<void>;
+
+export type DecryptFileFunction = (
+  algorithm: SymmetricCryptoAlgorithm,
+  key: BinaryData,
+  iv: BinaryData,
+  fileSize: number
+) => Promise<void>;
