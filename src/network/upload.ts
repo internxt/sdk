95dd0c21b3888bbe7e7fc7050b279eac9ad4a5da
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2022-06-13 16:25:39 +0200

    feat(upload): add method to uploadMultipartFile

--- src/network/upload.ts
+++ src/network/upload.ts
@@ -39,7 +39,59 @@ export async function uploadFile(
 
   const finishUploadPayload = {
     index: index.toString('hex'),
-    shards: [{ hash, uuid }]
+    shards: [{ hash, uuid }],
+  };
+
+  const finishUploadResponse = await network.finishUpload(bucketId, finishUploadPayload);
+
+  return finishUploadResponse.id;
+}
+
+export async function uploadMultipartFile(
+  network: Network,
+  crypto: Crypto,
+  bucketId: string,
+  mnemonic: string,
+  fileSize: number,
+  encryptFile: EncryptFileFunction,
+  uploadMultiparts: UploadFileMultipartFunction,
+  parts = 1,
+): Promise<string> {
+  const mnemonicIsValid = crypto.validateMnemonic(mnemonic);
+
+  if (!mnemonicIsValid) {
+    throw new UploadInvalidMnemonicError();
+  }
+
+  const index = crypto.randomBytes(crypto.algorithm.ivSize);
+  const iv = index.slice(0, 16);
+  const key = await crypto.generateFileKey(mnemonic, bucketId, index);
+
+  const { uploads } = await network.startUpload(
+    bucketId,
+    {
+      uploads: [
+        {
+          index: 0,
+          size: fileSize,
+        },
+      ],
+    },
+    parts,
+  );
+
+  const [{ urls, uuid, UploadId }] = uploads;
+
+  if (!urls) {
+    throw new UrlsNotReceivedFromNetworkError();
+  }
+
+  await encryptFile(crypto.algorithm.type, key, iv);
+  const { hash, parts: uploadedPartsReference } = await uploadMultiparts(urls);
+
+  const finishUploadPayload = {
+    index: index.toString('hex'),
+    shards: [{ hash, uuid, UploadId, parts: uploadedPartsReference }],
   };
 
   const finishUploadResponse = await network.finishUpload(bucketId, finishUploadPayload);
