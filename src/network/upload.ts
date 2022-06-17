672b2f4a155796cc0d3e6028d3703db627d603ec
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-06-17 10:54:26 +0200

    feat(network): use context on upload/download

--- src/network/upload.ts
+++ src/network/upload.ts
@@ -1,6 +1,6 @@
 import { Network } from '.';
-import { UploadInvalidMnemonicError } from './errors';
-import { Crypto, EncryptFileFunction, UploadFileFunction } from './types';
+import { UploadInvalidMnemonicError, ErrorWithContext, getNetworkErrorContext } from './errors';
+import { BinaryData, Crypto, EncryptFileFunction, UploadFileFunction } from './types';
 
 export async function uploadFile(
   network: Network,
@@ -11,34 +11,60 @@ export async function uploadFile(
   encryptFile: EncryptFileFunction,
   uploadFile: UploadFileFunction
 ): Promise<string> {
-  const mnemonicIsValid = crypto.validateMnemonic(mnemonic);
+  let index: BinaryData;
+  let iv: BinaryData;
+  let key: BinaryData;
 
-  if (!mnemonicIsValid) {
-    throw new UploadInvalidMnemonicError();
+  try {
+    const mnemonicIsValid = crypto.validateMnemonic(mnemonic);
+
+    if (!mnemonicIsValid) {
+      throw new UploadInvalidMnemonicError();
+    }
+
+    index = crypto.randomBytes(crypto.algorithm.ivSize);
+    iv = index.slice(0, 16);
+    key = await crypto.generateFileKey(mnemonic, bucketId, index);
+
+    const { uploads } = await network.startUpload(bucketId, {
+      uploads: [{
+        index: 0,
+        size: fileSize
+      }]
+    });
+
+    const [{ url, uuid }] = uploads;
+
+    await encryptFile(crypto.algorithm.type, key, iv);
+    const hash = await uploadFile(url);
+
+    const finishUploadPayload = {
+      index: index.toString('hex'),
+      shards: [{ hash, uuid }]
+    };
+
+    const finishUploadResponse = await network.finishUpload(bucketId, finishUploadPayload);
+
+    return finishUploadResponse.id;
+  } catch (err) {
+    const context = getNetworkErrorContext({
+      bucketId,
+      fileSize,
+      user: network.credentials.username,
+      pass: network.credentials.password,
+      crypto: {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        index: index! ? index.toString('hex') : 'none',
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        iv: iv! ? iv.toString('hex') : 'none',
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        key: key! ? key.toString('hex') : 'none',
+        mnemonic
+      }
+    }, err as Error);
+
+    (err as ErrorWithContext).context = context;
+
+    throw err;
   }
-
-  const index = crypto.randomBytes(crypto.algorithm.ivSize);
-  const iv = index.slice(0, 16);
-  const key = await crypto.generateFileKey(mnemonic, bucketId, index);
-
-  const { uploads } = await network.startUpload(bucketId, {
-    uploads: [{
-      index: 0,
-      size: fileSize
-    }]
-  });
-
-  const [{ url, uuid }] = uploads;
-
-  await encryptFile(crypto.algorithm.type, key, iv);
-  const hash = await uploadFile(url);
-
-  const finishUploadPayload = {
-    index: index.toString('hex'),
-    shards: [{ hash, uuid }]
-  };
-
-  const finishUploadResponse = await network.finishUpload(bucketId, finishUploadPayload);
-
-  return finishUploadResponse.id;
 }
