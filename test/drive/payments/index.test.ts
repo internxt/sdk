06af39724d460974c59cbc440efe5362b45b3e18
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-01-31 11:16:27 +0100

    tests: getInvoices

--- test/drive/payments/index.test.ts
+++ test/drive/payments/index.test.ts
@@ -2,7 +2,7 @@ import sinon from 'sinon';
 import { ApiSecurity, AppDetails } from '../../../src/shared';
 import { headersWithToken } from '../../../src/shared/headers';
 import { Payments } from '../../../src/drive';
-import { CreatePaymentSessionPayload, StripeSessionMode } from '../../../src/drive/payments/types';
+import { CreatePaymentSessionPayload, StripeSessionMode, UserType } from '../../../src/drive/payments/types';
 import { HttpClient } from '../../../src/shared/http/client';
 
 const httpClient = HttpClient.create('');
@@ -32,6 +32,61 @@ describe('payments service', () => {
       expect(body).toEqual({
         content: 'true',
       });
+    });
+  });
+
+  describe('getInvoices', () => {
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should call with right params & return data', async () => {
+      const callStub = sinon.stub(httpClient, 'get').resolves([
+        { id: 'invoice_123', amount: 1000 },
+        { id: 'invoice_456', amount: 2000 },
+      ]);
+
+      const { client, headers } = clientAndHeadersWithToken();
+
+      const params = {
+        subscriptionId: 'sub_789',
+        startingAfter: 'invoice_123',
+        userType: UserType.Individual,
+        limit: 10,
+      };
+
+      const invoices = await client.getInvoices(params);
+
+      const expectedQuery = new URLSearchParams({
+        subscription: 'sub_789',
+        starting_after: 'invoice_123',
+        userType: UserType.Individual,
+        limit: '10',
+      }).toString();
+
+      expect(callStub.firstCall.args).toEqual([`/invoices?${expectedQuery}`, headers]);
+
+      expect(invoices).toEqual([
+        { id: 'invoice_123', amount: 1000 },
+        { id: 'invoice_456', amount: 2000 },
+      ]);
+    });
+
+    it('should handle missing optional parameters correctly', async () => {
+      const callStub = sinon.stub(httpClient, 'get').resolves([]);
+
+      const { client, headers } = clientAndHeadersWithToken();
+
+      const params = {};
+
+      const expectedQuery = new URLSearchParams({
+        userType: UserType.Individual,
+      }).toString();
+
+      const invoices = await client.getInvoices(params as any);
+
+      expect(callStub.firstCall.args).toEqual([`/invoices?${expectedQuery}`, headers]);
+      expect(invoices).toEqual([]);
     });
   });
 
