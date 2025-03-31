237e15bb80996672283190f717c40b50a4b8e214
jzunigax2 <125698953+jzunigax2@users.noreply.github.com>
2025-03-31 09:13:36 -0600

    chore: improve test coverage

--- test/auth/index.test.ts
+++ test/auth/index.test.ts
@@ -129,7 +129,7 @@ describe('# auth service tests', () => {
           keys: {
             ecc: {
               publicKey: registerDetails.keys.ecc.publicKey,
-              privateKey: registerDetails.keys.ecc.privateKeyEncrypted,   
+              privateKey: registerDetails.keys.ecc.privateKeyEncrypted,
             },
             kyber: {
               publicKey: registerDetails.keys.kyber.publicKey,
@@ -550,6 +550,56 @@ describe('# auth service tests', () => {
       // Assert
       expect(callStub.firstCall.args).toEqual(['users/unblock-account', { token }, headers]);
       expect(body).toEqual({});
+    });
+  });
+
+  describe('-> change password with link', () => {
+    it('Should call with right params without private keys', async () => {
+      const callStub = sinon.stub(httpClient, 'put').resolves({});
+      const { client, headers } = clientAndHeaders();
+      const token = 'token';
+      const password = 'newPassword';
+      const salt = 'newSalt';
+      const mnemonic = 'newMnemonic';
+
+      await client.changePasswordWithLink(token, password, salt, mnemonic);
+
+      expect(callStub.firstCall.args).toEqual([
+        `/users/recover-account?token=${token}&reset=false`,
+        {
+          password,
+          salt,
+          mnemonic,
+        },
+        headers,
+      ]);
+    });
+
+    it('Should call with right params including private keys', async () => {
+      const callStub = sinon.stub(httpClient, 'put').resolves({});
+      const { client, headers } = clientAndHeaders();
+      const token = 'token';
+      const password = 'newPassword';
+      const salt = 'newSalt';
+      const mnemonic = 'newMnemonic';
+      const privateKeys = {
+        ecc: 'newEccKey',
+        kyber: 'newKyberKey',
+      };
+
+      await client.changePasswordWithLink(token, password, salt, mnemonic, privateKeys);
+
+      // Assert
+      expect(callStub.firstCall.args).toEqual([
+        `/users/recover-account?token=${token}&reset=false`,
+        {
+          password,
+          salt,
+          mnemonic,
+          privateKeys,
+        },
+        headers,
+      ]);
     });
   });
 });
