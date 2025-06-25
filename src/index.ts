90697fc52b9efe1ea7cb9b447dcb05c7b1068314
larryrider <larryrider@users.noreply.github.com>
2025-06-25 15:46:33 +0200

    refactor: remove unused photos module and related submodules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from './auth';
 export * as Drive from './drive';
 export * as Network from './network';
-export * as photos from './photos';
 export * as Shared from './shared';
 export * as Workspaces from './workspaces';
 export * from './meet';
