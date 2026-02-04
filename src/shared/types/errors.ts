65dacd0ba6283866837f507930fe71a549432c16
Francis Terrero <terrero0611@gmail.com>
2026-02-03 23:28:32 -0400

    feat(errors): restructure AppError to include response headers in a nested object

--- src/shared/types/errors.ts
+++ src/shared/types/errors.ts
@@ -1,13 +1,15 @@
 export default class AppError extends Error {
   public readonly status?: number;
   public readonly code?: string;
-  public readonly headers?: Record<string, string>;
+  public readonly response?: {
+    headers?: Record<string, string>;
+  };
 
   constructor(message: string, status?: number, code?: string, headers?: Record<string, string>) {
     super(message);
 
     this.status = status;
     this.code = code;
-    this.headers = headers;
+    this.response = headers ? { headers } : undefined;
   }
 }
