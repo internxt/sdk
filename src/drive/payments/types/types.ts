d0525771d63f15c8983b121400a8fba6ce912aa6
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2026-05-05 09:23:25 +0200

    feat(payments): add commitment object for user subscriptions

--- src/drive/payments/types/types.ts
+++ src/drive/payments/types/types.ts
@@ -81,6 +81,11 @@ export type StoragePlan = {
   isTeam: boolean;
   isLifetime: boolean;
   renewalPeriod: RenewalPeriod;
+  commitment: {
+    enabled: boolean;
+    remainingMonths: number;
+    cancellationDate: string | null;
+  };
   storageLimit: number;
   amountOfSeats: number;
   isAppSumo?: boolean;
