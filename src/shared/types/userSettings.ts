c84c569f2483632bd833f3f49c26a90a2d42f232
tamarafinogina <tamara@internxt.com>
2026-07-09 12:43:23 +0200

    make old UserSettings fields optional

--- src/shared/types/userSettings.ts
+++ src/shared/types/userSettings.ts
@@ -18,9 +18,9 @@ export interface UserSettings {
   sharedWorkspace: boolean;
   credit: number;
   mnemonic: string;
-  privateKey: string;
-  publicKey: string;
-  revocationKey: string;
+  privateKey?: string;
+  publicKey?: string;
+  revocationKey?: string;
   keys: {
     ecc: {
       publicKey: string;
